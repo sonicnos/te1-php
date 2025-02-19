4b9d06030f1dd8e5196674eb6313c3750d5b9793
sonicnos <107223123+sonicnos@users.noreply.github.com>
2025-02-19 10:23:20 +0100

    connect js and css file

--- index.php
+++ index.php
@@ -8,11 +8,13 @@ include './public/index.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/public/css/style.css">
   <title><?php echo $title ?></title>
 </head>
 
 <body>
   <?php include "./templates/inc/menu.php" ?>
+  <script src="/public/js/script.js"></script>
 </body>
 
 </html>
